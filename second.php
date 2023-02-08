d96c5c20dd1f60f6fd8924d19941958aef154046
Drashti <drashtisevara@gmail.com>
2023-02-08 11:14:43 +0530

    adding second.php

---
+++ second.php
@@ -0,0 +1,15 @@
+<?php
+
+function functionName($name = "piyu"   ,   $surname = "Pandya"){
+    echo "This is  a  $name $surname . <br>";
+}
+functionName();
+
+functionName("Sakshi" , "Vadhiya");
+
+functionName("Bavli" , "Vadhiya");
+
+
+
+
+?>
