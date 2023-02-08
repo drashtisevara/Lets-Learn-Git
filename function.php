2eefe311c33334f9c20d105eb624d421b8afd6c3
Drashti <drashtisevara@gmail.com>
2023-02-08 10:37:50 +0530

    initial commit

---
+++ function.php
@@ -0,0 +1,29 @@
+<?php
+
+function hellow(){
+    echo " Hii  helooow everyone how are you" . "<br>";
+
+}
+
+function yahoo(){
+    echo "  drashti hii how are you" . "<br>";
+
+}
+hellow();
+
+hellow();
+
+hellow();
+
+yahoo();
+
+echo "This is an example";
+
+hellow();
+
+hellow();
+
+
+
+
+?>
